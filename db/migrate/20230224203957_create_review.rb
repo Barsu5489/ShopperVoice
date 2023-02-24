4d582f085c4b9c5d571350dc0186f8e373804180
barsu5489 <ebarsulai@gmail.com>
2023-02-24 23:50:01 +0300

    created review table - join table

--- db/migrate/20230224203957_create_review.rb
+++ db/migrate/20230224203957_create_review.rb
@@ -1,4 +1,10 @@
 class CreateReview < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :comment
+      t.integer :star_rating
+      t.integer :product_id
+      t.integer :user_id
+    end
   end
 end
