966444b385ca71681ce7530926249207d0f42905
barsu5489 <ebarsulai@gmail.com>
2023-02-25 18:18:16 +0300

    product and user collection

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,11 @@
 class Product < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
+
+    def product_reviews
+        reviews
+    end
+    def product_users
+        users
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+
+    def user_reviews
+        reviews
+    end
+
+    def user_products
+        products
+    end
 end
