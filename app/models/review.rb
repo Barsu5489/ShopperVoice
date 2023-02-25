e0f68875c228ebad1c91fb91518f48c2915bae35
barsu5489 <ebarsulai@gmail.com>
2023-02-25 18:04:36 +0300

    users that have reviewd and reviews made on products

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,12 @@
 class Review < ActiveRecord::Base
     belongs_to :product
     belongs_to :user
+
+
+    def review_user
+        user
+    end
+    def review_product
+        product
+    end
 end
