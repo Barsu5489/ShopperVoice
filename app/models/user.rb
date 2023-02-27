a1a4a39e7a36bfbc46ff3af6eea20c6040d91b2c
barsu5489 <ebarsulai@gmail.com>
2023-02-27 14:23:28 +0300

    remove product

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,4 +14,7 @@ class User < ActiveRecord::Base
     def favorite_product
         reviews.order(star_rating: :desc).first.product
       end
+    def remove_reviews(product)
+        reviews.where(product_id: product.id).destroy_all
+    end
 end
